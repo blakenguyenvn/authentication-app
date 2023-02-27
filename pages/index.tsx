32f79e3412cf0bc3782775cc2bd17f6295b5c8e1
Blake Nguyen <blake.nguyen@theaccessgroup.com>
2023-02-27 16:01:05 +0700

    [Initial] NextJS, Tailwind, Eslint, Prettier

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import styles from '@/styles/Home.module.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+        <meta name="description" content="Authentication App" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <h1 class="text-3xl font-bold underline">Authentication Application</h1>
+      </main>
+    </>
+  );
+}
